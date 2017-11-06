cef04f682642b04adde22a071e1dc80a8df5e82f
sunliang <sunliang@edt.com>
2017-11-06 17:32:36 +0800

    测试websocket

--- WebRoot/WEB-INF/view/gzh/addgzhInfo.jsp
+++ WebRoot/WEB-INF/view/gzh/addgzhInfo.jsp
@@ -1,4 +1,4 @@
-﻿﻿<%@ page language="java" import="java.util.*" pageEncoding="UTF-8" %>
+﻿<%@ page language="java" import="java.util.*" pageEncoding="UTF-8" %>
 <%@include file="../common.jsp" %>
 <!DOCTYPE HTML>
 <html>
---
+++ src/com/edt/websocket/handler/DemoWsHandler.java
@@ -0,0 +1,37 @@
+package com.edt.websocket.handler;
+
+import org.springframework.web.socket.*;
+
+public class DemoWsHandler implements WebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("connect to the websocket success......");
+        session.sendMessage(new TextMessage("Server:connected OK!"));
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession wss, WebSocketMessage<?> wsm) throws Exception {
+        TextMessage returnMessage = new TextMessage(wsm.getPayload()
+                + " received at server");
+        System.out.println(wss.getHandshakeHeaders().getFirst("Cookie"));
+        wss.sendMessage(returnMessage);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession wss, Throwable thrwbl) throws Exception {
+        if(wss.isOpen()){
+            wss.close();
+        }
+        System.out.println("websocket connection closed......");
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession wss, CloseStatus cs) throws Exception {
+        System.out.println("websocket connection closed......");
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
